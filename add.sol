ba1db9405483cdba0f772505b706d63cf39ec0e4
hanzala <135013774+hanzaladotjs@users.noreply.github.com>
2025-08-09 00:03:52 +0530

    Update add.sol

--- add.sol
+++ add.sol
@@ -1,1 +1,20 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+
+uint public x;
+
+constructor (uint _x){
+    x = _x;
+}
+
+
+function add(uint a) external view returns(uint){
+    return x+a;
+}
+
+
+  
+}
