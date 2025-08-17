0ebe778c6011a597503f76cb90e91c9300ca313d
hanzala <135013774+hanzaladotjs@users.noreply.github.com>
2025-08-17 20:55:36 +0530

    Create VoteStruct.sol

---
+++ VoteStruct.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Contract {
+	enum Choices { Yes, No }
+	
+	struct Vote {
+		Choices choice;
+		address voter;
+	}
+	
+	Vote[] public votes;
+
+	function createVote(Choices choice) external {
+		votes.push(Vote(choice, msg.sender));
+	}
+
+	function hasVoted(address adres) external view returns(bool) {
+		for(uint i; i<votes.length; i++){
+			if(votes[i].voter == adres){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function findChoice (address adres) external view returns(Choices) {
+		for(uint i; i<votes.length; i++){
+			if(votes[i].voter == adres){
+				if(votes[i].choice == Choices.Yes){
+					return Choices.Yes;
+				}
+			}
+		}
+		return Choices.No;
+	}
+}
